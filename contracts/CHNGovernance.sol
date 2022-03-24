0efc66a8c19a84e47193f150ec5e5bdfc197840e
ChainEngineer1 <74755843+ChainEngineer1@users.noreply.github.com>
2022-03-25 01:37:41 +1000

    update propose voting power validation

--- contracts/CHNGovernance.sol
+++ contracts/CHNGovernance.sol
@@ -57,7 +57,7 @@ contract CHNGovernance is CHNGovernanceStorage {
     }
 
     function propose(address[] memory targets, uint[] memory values, string[] memory signatures, bytes[] memory calldatas, string memory description) public returns (uint) {
-        require(getVotingPower(msg.sender, block.number) >= proposalThreshold, "GovernorAlpha::propose: proposer votes below proposal threshold");
+        require(getVotingPower(msg.sender, sub256(block.number, 1)) >= proposalThreshold, "GovernorAlpha::propose: proposer votes below proposal threshold");
         require(targets.length == values.length && targets.length == signatures.length && targets.length == calldatas.length, "GovernorAlpha::propose: proposal function information arity mismatch");
         require(targets.length != 0, "GovernorAlpha::propose: must provide actions");
         require(targets.length <= proposalMaxOperations, "GovernorAlpha::propose: too many actions");
